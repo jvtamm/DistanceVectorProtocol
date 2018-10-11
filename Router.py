299b59bd9c3e8c39b81043aaf00d981a6143f81f
jvtamm <jv_tamm@hotmail.com>
2018-10-11 11:28:48 -0300

    Finished handling link creation

--- Router.py
+++ Router.py
@@ -7,12 +7,80 @@
 # http://www.mathcs.emory.edu/~cheung/Courses/558/Syllabus/13-Routing/distance-vec1.html
 # https://codereview.stackexchange.com/questions/83790/pydos-shell-simulation
 # https://github.com/gu9/testpython-/blob/master/pg01_gu9.py
-import socket, struct
+import socket, sys, json
+from collections import defaultdict as dd
+from threading import Lock
 
 class Router:
     def __init__(self, addr, period):
-        self.table = dict() # Key -> destination addr | Value -> Tuple (weight, nextHop)
+        self.routing_table = dd(lambda: dd(list)) # Key -> destination addr | Value -> [Tuple (distance, nextHop)]
+        self.link_table = dict()    # Key -> destination | Value -> Weight
         self.period = period    # Might be deleted when timer is created
+
+        self.link_lock = Lock()
+        self.routing_lock = Lock()
 
         self.udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.udp.bind((addr, 55151))
+
+    def add_link(self, addr, weight):
+        if (addr == self.udp.getsockname()[0]): sys.exit("Tried to add a link to itself")
+
+        self.link_lock.acquire()
+        self.link_table[addr] = int(weight)
+        self.link_lock.release()
+
+    def remove_link(self, addr):
+        self.link_lock.acquire()
+        # Adicionar remocao de todas as rotas via o hop
+        del self.link_table[addr]
+        self.link_lock.release()
+
+    def trace(self, ip):
+        print(self)
+        print('Should implement trace function')
+
+    def handle_messages(self):
+        actions = {'data': self.handle_data, 'update': self.handle_update, 'trace': self.handle_trace}
+        # data = json.dumps({"source": "127.0.0.4", "destination": "127.0.0.2", "type": "data", "payload": "{\"source\": \"127.0.0.2\", \"destination\": \"127.0.0.4\", \"type\": \"trace\", \"hops\": [\"127.0.0.1\", \"127.0.0.4\"]}"})
+        data = json.dumps({ "type": "update","source": "127.0.1.5","destination": "127.0.0.1","distances": {"127.0.1.4": 10,"127.0.1.5": 0,"127.0.1.2": 10,"127.0.1.3": 10}})
+        formated_data = json.loads(data)
+
+        if(formated_data['type'] in actions):
+            action = actions[formated_data['type']]
+            action(formated_data)
+    
+    def handle_data(self, data):
+        if (data['destination'] == self.udp.getsockname()[0]): 
+            print(data['payload'])
+        else:
+            print('Should check routing table, get shortest route, and send to nextHop')
+
+    def handle_update(self, data):
+        if (data['destination'] != self.udp.getsockname()[0]):
+            print('Destination is not this router')
+            return
+
+        self.link_lock.acquire()
+        self.routing_lock.acquire()
+        
+        if (not data['source'] in self.link_table):
+            print('Update message being sent through a non existing link')
+            self.link_lock.release()
+            self.routing_lock.release()
+            return
+        
+        # Adicionar remoção de todas as rotas que foram recebidas via aquele hop
+
+        print(data['distances'])
+        for key, value in data['distances'].items():
+            distance = int(value) + self.link_table[data['source']]
+            self.routing_table[key][data['source']].append(distance)
+
+        print(vars(self))
+
+        self.link_lock.release()
+        self.routing_lock.release()
+
+    def handle_trace(self, data):
+        print(data)
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+#! /usr/bin/python3
+
 import argparse
+from threading import Thread
 
 from Router import Router
 
@@ -8,10 +11,22 @@ def input_parser():
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--addr', action = 'store', dest = 'addr', required = True, help = 'Router address')
+    # parser.add_argument("addr", metavar="ADDR", type=str, help = 'Router address')
+
     parser.add_argument('--update-period', action = 'store', dest = 'period', required = True, help = 'Router update period')
+    # parser.add_argument("period", metavar="PERIOD", type=str, help = 'Router update period')
+
     parser.add_argument('--startup-commands', action='store', dest='init_file', default= None, required= False, help = 'File to initialize topology')
+    # parser.add_argument("init_file", metavar="STARTUP", nargs="?", type=str, help = 'File to initialize topology')
 
     return parser.parse_args()
+
+def handle_commands(command, router):
+    cmd = command.split()
+    if(cmd[0] == 'add'): router.add_link(cmd[1], cmd[2])
+    elif(cmd[0] == 'del'): router.remove_link(cmd[1])
+    elif(cmd[0] == 'trace'): router.trace(cmd[1])
+    else: print('Invalid command')
 
 def main():
     arguments = input_parser()
@@ -20,13 +35,20 @@ def main():
     if (arguments.init_file != None):
         with open(arguments.init_file) as fp:
             for line in fp:
-                # Handle topology through file 
-                print(line)
+                cmd = line.split()
+                handle_commands(cmd, router)
 
+    # router.handle_messages()
 
-    print(vars(router))
+    # Trigger thread to receive messages
+    # message_thread = Thread(target = router.handle_messages)
 
+    # Create infinite loop to handle keyboard commands
     while(True):
+        cmd = input()
+        handle_commands(cmd, router)
+        router.handle_messages()
+        print(router.routing_table)
         
 
 if __name__ == '__main__':
